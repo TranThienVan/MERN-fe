fd6f898d720cb09ea42a97deab061b4089f645cd
TranThienVan <s3740845@rmit.edu.vn>
2021-09-24 18:15:11 +0700

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,77 @@
+import React, { useContext } from 'react';
+import './topbar.css';
+import { Search, Person, Home, Telegram, Notifications, ExitToApp, LaptopWindows } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+
+const Topbar = () => {
+	const { user } = useContext(AuthContext);
+
+	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+	return (
+		<div className="topbarContainer">
+			{/* TopbarLeft */}
+			<div className="topbarLeft">
+				<Link to="/" style={{ color: 'black', textDecoration: ' none' }}>
+					<span className="logo">Homebook</span>
+				</Link>
+			</div>
+
+			{/* TopbarCenter */}
+			<div className="topbarCenter">
+				{/* Searchbar */}
+				<div className="searchbar">
+					<Search className="searchIcon" />
+					<input placeholder="Search" className="searchInput" />
+				</div>
+			</div>
+
+			{/* TopbarRight */}
+			<div className="topbarRight">
+				<div className="topbarLinks">
+					<Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
+						<Home className="topbarLink1" />
+					</Link>
+
+					<Link
+						to="/login"
+						style={{ textDecoration: 'none', color: 'black' }}
+						onClick={(e) => {
+							e.preventDefault();
+							localStorage.clear();
+							window.location.reload();
+						}}
+					>
+						<ExitToApp className="topbarLink" />
+					</Link>
+				</div>
+				<div className="topbarIcons">
+					<div className="topbarIconItem">
+						<Person className="topbarleftIcons" />
+						<span className="topbarIconBadge">3</span>
+					</div>
+					<div className="topbarIconItem">
+						<Link to="/messenger" style={{ textDecoration: 'none', color: 'black' }}>
+							<Telegram className="topbarleftIcons" />
+							<span className="topbarIconBadge">2</span>
+						</Link>
+					</div>
+					<div className="topbarIconItem">
+						<Notifications className="topbarleftIcons" />
+						<span className="topbarIconBadge">3</span>
+					</div>
+				</div>
+				<Link to={`/profile/${user.username}`}>
+					<img
+						src={user.profilePicture ? PF + user.profilePicture : PF + 'person/noAvatar.png'}
+						alt=""
+						className="topbarImg"
+					/>
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Topbar;
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
+import './register.css';
+
+const Register = () => {
+	// Quickly form require
+	const username = useRef();
+	const email = useRef();
+	const password = useRef();
+	const passwordAgain = useRef();
+
+	const history = useHistory();
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+		if (passwordAgain.current.value !== password.current.value) {
+			passwordAgain.current.setCustomValidity("Passwords don't match!");
+		} else {
+			const user = {
+				username: username.current.value,
+				email: email.current.value,
+				password: password.current.value
+			};
+			try {
+				await axios.post('/auth/register', user);
+				history.push('/login');
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	};
+
+	return (
+		<div className="login">
+			<div className="loginWrapper">
+				<div className="loginLeft">
+					<h3 className="loginLogo">Homebook</h3>
+					<span className="loginDesc">Connect with friends and the world around you! </span>
+				</div>
+				<div className="loginRight">
+					<form className="loginBox" onSubmit={handleClick}>
+						<input placeholder="Username" required ref={username} className="loginInput" />
+						<input placeholder="Email" required ref={email} type="email" className="loginInput" />
+						<input
+							placeholder="Password"
+							required
+							ref={password}
+							type="password"
+							minLength="6"
+							className="loginInput"
+						/>
+						<input
+							placeholder="Re-enter Password"
+							required
+							ref={passwordAgain}
+							type="password"
+							className="loginInput"
+						/>
+						<button className="loginButton" type="submit">
+							Sign up
+						</button>
+						<Link to="/login" style={{ width: '100%' }}>
+							<button className="loginRegisterButton">Login in Account</button>
+						</Link>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
